70842ec21fb59dd96c7c5a6f9535b6bb98378c85
201710787 <egnachio@naver.com>
2023-05-02 22:38:45 +0900

    FEAT: main ui

--- src/pages/content/main.tsx
+++ src/pages/content/main.tsx
@@ -17,9 +17,11 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {useRouter} from 'next/router'
 import { useAppDispatch, useAppSelector, setInfo, addTag, increaseTagCount } from "@/components/store";
 import {Badge, Chip, Divider, IconButton, InputBase, Paper} from "@mui/material";
-import {Menu, Search, Directions} from "@mui/icons-material";
+import {Menu, Search, Directions, Favorite, Face} from "@mui/icons-material";
 import MiniDrawer from "@/components/drawer";
 import {useState} from "react";
+import MainModal from "@/components/modal/mainModal";
+import * as React from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -34,7 +36,7 @@ export default function Home() {
     const router = useRouter()
     const dispatch = useAppDispatch()
     const info: any | Info = useAppSelector(state => state.userInfo)
-    const [tag, setTag] = useState('')
+
     const handleClick = (...rest: any[]) => {
         const index = rest[0]
         dispatch(increaseTagCount(index))
@@ -69,33 +71,9 @@ export default function Home() {
                         margin: 20
                     }}/>
                     <Typography variant="h6" gutterBottom>
-                        좋아!
+                        소소식탁
                     </Typography>
-                    <Paper
-                        component="form"
-                        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 4/5 }}
-                    >
-                        <InputBase
-                            sx={{ ml: 1, flex: 1 }}
-                            placeholder="add"
-                            value={tag}
-                            inputProps={{ 'aria-label': 'add' }}
-                            onChange={(e) => setTag(e.target.value)}
-                        />
-                        <IconButton
-                            type="button"
-                            sx={{ p: '10px' }}
-                            aria-label="search"
-                            onClick={()=>{
-                                dispatch(addTag(tag))
-                                setTag('')
-                            }}
-                        >
-                            <Search
-
-                            />
-                        </IconButton>
-                    </Paper>
+                    <MainModal/>
                     <Box sx={{
                         display: 'flex',
                         flexWrap: 'wrap',
@@ -108,6 +86,7 @@ export default function Home() {
                             margin: 10
                         }}>
                             <Chip
+                                icon={<Face />}
                                 label="팥붕"
                                 onClick={handleClick}
                                 onDelete={handleDelete}
@@ -176,80 +155,7 @@ export default function Home() {
                             ))
                         }
                     </Box>
-                    <Typography variant="h6" gutterBottom>
-                        싫어...
-                    </Typography>
-                    <Paper
-                        component="form"
-                        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 4/5 }}
-                    >
-                        <InputBase
-                            sx={{ ml: 1, flex: 1 }}
-                            placeholder="add"
-                            value={tag}
-                            inputProps={{ 'aria-label': 'add' }}
-                            onChange={(e) => setTag(e.target.value)}
-                        />
-                        <IconButton
-                            type="button"
-                            sx={{ p: '10px' }}
-                            aria-label="search"
-                            onClick={()=>{
-                                dispatch(addTag(tag))
-                                setTag('')
-                                console.log(info.tag)
-                            }}
-                        >
-                            <Search
 
-                            />
-                        </IconButton>
-                    </Paper>
-                    <Box sx={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        margin: '10px'
-                    }}>
-                        <Badge badgeContent={4} color="primary" style={{
-                            margin: 10
-                        }}>
-                            <Chip
-                                label="익힌 채소"
-                                onClick={handleClick}
-                                onDelete={handleDelete}
-                            />
-                        </Badge>
-                        <Badge badgeContent={4} color="primary" style={{
-                            margin: 10
-                        }}>
-                            <Chip
-                                label="굴"
-                                onClick={handleClick}
-                                onDelete={handleDelete}
-                            />
-                        </Badge>
-                        <Badge badgeContent={4} color="primary" style={{
-                            margin: 10
-                        }}>
-                            <Chip
-                                label="마늘"
-                                onClick={handleClick}
-                                onDelete={handleDelete}
-                            />
-                        </Badge>
-                        <Badge badgeContent={4} color="primary" style={{
-                            margin: 10
-                        }}>
-                            <Chip
-                                label="서있기(허리아픔)"
-                                onClick={handleClick}
-                                onDelete={handleDelete}
-                            />
-                        </Badge>
-                    </Box>
                 </Box>
             </main>
         </>
