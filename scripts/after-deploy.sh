3b0fd1e3e3e5cf982f42063ef7d932fbfab5cace
Ujin28 <melon4092@naver.com>
2023-05-23 20:42:51 +0900

    ci-cd

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -12,5 +12,5 @@ sudo npm i
 # 참조
 # https://unchae.tistory.com/entry/PM2-80-443%ED%8F%AC%ED%8A%B8-%EC%82%AC%EC%9A%A9
 #authbind --deep pm2 reload ./src/pages/index.tsx --watch
-sudo npm run build
+npm run build
 pm2 start deploy.json
